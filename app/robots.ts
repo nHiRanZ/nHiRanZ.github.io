1a76bffb942054ee950468722e77d08679679b35
Nimila Hiranya Samarasinghe <hellochefprabath@gmail.com>
2026-02-22 01:16:17 -0600

    SEO Fix

--- app/robots.ts
+++ app/robots.ts
@@ -2,6 +2,8 @@ import { MetadataRoute } from 'next'
 
 const SITE_URL = 'https://nimila.online'
 
+export const dynamic = 'force-static'
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
