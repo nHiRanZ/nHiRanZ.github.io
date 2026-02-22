7a84ab541d0720095e0a7d7c845b2d120a545d0d
Nimila Hiranya Samarasinghe <hellochefprabath@gmail.com>
2026-02-22 01:35:23 -0600

    Update clarity-provider.tsx

--- components/clarity-provider.tsx
+++ components/clarity-provider.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useEffect } from "react"
-import { clarity } from "@microsoft/clarity"
+import clarity from "@microsoft/clarity"
 
 const CLARITY_PROJECT_ID = "vl90vwyl05"
 
